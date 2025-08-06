4cd1ffda736220923530b1f9ed210f82a9d35a75
arul <arulkumaran1281@gmail.com>
2025-08-06 09:21:44 +0530

    NA

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,8 +1,19 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        int arr[]={19,3,4,12,8};
-
+        int arr[]={19,3,4,12,8},n=5;
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
---
+++ src/sorting/InsersionSort.java
@@ -0,0 +1,8 @@
+package sorting;
+
+public class InsersionSort {
+    public static void main(String[] args) {
+        int arr[]={19,3,4,12,8},n=5;
+
+    }
+}
