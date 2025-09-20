c954ba98616ba8c5ad34d6c75067a52e0604878c
arul <arulkumaran1281@gmail.com>
2025-09-20 13:12:24 +0530

    java file

--- src/arrays/LargestElement.java
+++ src/arrays/LargestElement.java
@@ -2,8 +2,8 @@ package arrays;
 
 public class LargestElement {
     public static void main(String[] args) {
-        int arr[]={2,5,1,3,0},n= arr[1];
-        for(int i=0;i< arr.length-1;i++){
+        int arr[]={100,2,5,1,3,0,15,20},n= arr[1];
+        for(int i=0;i< arr.length;i++){
             if(n<arr[i]){
                 n=arr[i];
             }
